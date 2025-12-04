dcafdae5121e5a5008058934b94dea2ea82009b5
huy2222 <nguyennhathuy21112005@gmail.com>
2025-12-04 22:38:44 +0700

    change Customer

--- src/main/java/org/example/tourtour/entity/Customer.java
+++ src/main/java/org/example/tourtour/entity/Customer.java
@@ -1,4 +1,18 @@
 package org.example.tourtour.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+//@Entity
 public class Customer {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String name;
+//    private String email;
+//    private String phoneNumber;
 }
