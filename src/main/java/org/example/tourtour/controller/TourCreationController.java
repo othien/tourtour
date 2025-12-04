2a4fca8688f7bdc50c4bedf3a02d93d35039b41d
huy2222 <nguyennhathuy21112005@gmail.com>
2025-12-04 21:33:11 +0700

    add TourCreationControllercontroller

---
+++ src/main/java/org/example/tourtour/controller/TourCreationController.java
@@ -0,0 +1,9 @@
+package org.example.tourtour.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tours")
+public class TourCreationController {
+}
