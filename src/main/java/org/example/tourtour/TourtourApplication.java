1555a4ee8b64c38e664d3ae954986a22ca52c4bb
othien <odhthien29@gmail.com>
2025-12-04 20:38:17 +0700

    Initial commit - Setup Spring Boot structure and entities

---
+++ src/main/java/org/example/tourtour/TourtourApplication.java
@@ -0,0 +1,13 @@
+package org.example.tourtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourtourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourtourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tourtour/TourtourApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tourtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourtourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
